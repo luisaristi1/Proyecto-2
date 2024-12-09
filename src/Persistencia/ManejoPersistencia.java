c8764394b6cedef6bccca02a19ee417dd59c13c3
nierazo <141872215+nierazo@users.noreply.github.com>
2024-12-08 23:11:36 -0500

    mini 3

--- src/Persistencia/ManejoPersistencia.java
+++ src/Persistencia/ManejoPersistencia.java
@@ -55,6 +55,17 @@ public class ManejoPersistencia {
 		}
 		return rta;
 	}
+
+	public Boolean verificarProfesorExistente(Profesor usuario) {
+		Boolean rta = false; //por defecto se asume que no está
+		for (String usuar: mapaProfesores.keySet()) {
+			Profesor usu = mapaProfesores.get(usuar);
+			if (usu.equals(usuario)) {
+				rta = true;
+			}
+		}
+		return rta;
+	}
 	
 
 	//SECCI0N PARA ACTIVIDADES
